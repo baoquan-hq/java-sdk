bfb42aa3f0597caf136b9272dd41701c536f0bc9
sixone <sixone1@qq.com>
2019-12-18 18:01:33 +0800

    init 3.0.0

--- src/main/java/com/baoquan/sdk/RequestIdGenerator.java
+++
@@ -1,9 +0,0 @@
-package com.baoquan.sdk;
-
-/**
- * Created by sbwdlihao on 6/17/16.
- */
-public interface RequestIdGenerator {
-
-  String createRequestId();
-}
--- src/main/java/com/baoquan/sdk/pojos/payload/CaType.java
+++
@@ -1,9 +0,0 @@
-package com.baoquan.sdk.pojos.payload;
-
-/**
- * Created by sbwdlihao on 6/17/16.
- */
-public enum CaType {
-  PERSONAL,
-  ENTERPRISE
-}
